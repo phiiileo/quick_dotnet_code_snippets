868ed7d928bda33d39bb983b1228f67ec1aa5fee
Phiiileo <emmanuelboluwajib1@gmail.com>
2020-08-31 13:17:50 +0100

    setup a console app for test running code snippets

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace quick_code_run
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
